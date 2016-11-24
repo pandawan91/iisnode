1c620e53d9de6290e3ebb9e07ede95389d2b5045
Vuong Ngo <v.ngo@cpc-baulogistik.de>
2016-11-24 16:19:24 +0100

    zip build

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,39 @@
-var gulp = require('gulp'),
-  nodemon = require('gulp-nodemon'),
-  plumber = require('gulp-plumber'),
-  livereload = require('gulp-livereload');
+var gulp = require('gulp');
+var path = require('path');
+var zip = require('gulp-zip');
+var minimist = require('minimist');
+var fs = require('fs');
 
+var knownOptions = {
+	string: 'packageName',
+	string: 'packagePath',
+	default: {packageName: "Package.zip", packagePath: path.join(__dirname, '_package')}
+}
 
-gulp.task('develop', function () {
-  livereload.listen();
-  nodemon({
-    script: 'bin/www',
-    ext: 'js jade coffee',
-    stdout: false
-  }).on('readable', function () {
-    this.stdout.on('data', function (chunk) {
-      if(/^Express server listening on port/.test(chunk)){
-        livereload.changed(__dirname);
-      }
-    });
-    this.stdout.pipe(process.stdout);
-    this.stderr.pipe(process.stderr);
-  });
+var options = minimist(process.argv.slice(2), knownOptions);
+
+gulp.task('default', function () {
+
+	var packagePaths = ['**',
+					'!**/_package/**',
+					'!**/typings/**',
+					'!typings',
+					'!_package',
+					'!gulpfile.js']
+
+	//add exclusion patterns for all dev dependencies
+	var packageJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
+	var devDeps = packageJSON.devDependencies;
+
+	for(var propName in devDeps)
+	{
+		var excludePattern1 = "!**/node_modules/" + propName + "/**";
+		var excludePattern2 = "!**/node_modules/" + propName;
+		packagePaths.push(excludePattern1);
+		packagePaths.push(excludePattern2);
+	}
+
+    return gulp.src(packagePaths)
+        .pipe(zip(options.packageName))
+        .pipe(gulp.dest(options.packagePath));
 });
-
-gulp.task('default', [
-  'develop'
-]);
